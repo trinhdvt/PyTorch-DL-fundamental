5c26dbf82076448f639e3647dea8f7912d816c81
trinhdvt <trinhvideo123@gmail.com>
2021-11-01 22:55:07 +0700

    fix wrong forward method

--- flower-classification/core/build_model.py
+++ flower-classification/core/build_model.py
@@ -1,20 +1,19 @@
 from torch import nn
-import torch.nn.functional as F
 import random
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=1):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding='same'):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels,
-                              kernel_size, stride, padding)
+                              kernel_size, stride, padding=padding)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.conv(x)
-        x = self.bn(x)
         x = self.relu(x)
+        x = self.bn(x)
         return x
 
 
@@ -28,8 +27,8 @@ class CNN(nn.Module):
             [64, 128, 3],
             [128, 256, 3]
         ]
-        pooled_size = input_size / 2**len(conv_params)
-        dropout=(0, 0.2, 0.3, 0.5)
+        pooled_size = input_size // 2**len(conv_params)
+        dropout = (0, 0.2, 0.3, 0.5)
         classifier_params = [
             [256*pooled_size*pooled_size, 512],
             [512, 128],
@@ -37,12 +36,13 @@ class CNN(nn.Module):
             [64, num_classes]
         ]
 
-        self.classifier = self.make_classifier(classifier_params, dropout=dropout)
+        self.classifier = self.make_classifier(
+            classifier_params, dropout=dropout)
         self.conv = self.make_backbone(conv_params)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        x = self.conv(x)
+        x = self.classifier(x)
         return x
 
     @staticmethod
