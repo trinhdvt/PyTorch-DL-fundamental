6191d7759ae926a4a2808dd3047ebf8c7615c589
trinhdvt <trinhvideo123@gmail.com>
2021-11-02 00:42:14 +0700

    modified model structure

--- flower-classification/core/build_model.py
+++ flower-classification/core/build_model.py
@@ -3,17 +3,25 @@ import random
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding='same'):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size, batch_norm=True,
+                 stride=1, padding='same'):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels,
                               kernel_size, stride, padding=padding)
-        self.bn = nn.BatchNorm2d(out_channels)
+        if batch_norm:
+            self.bn = nn.BatchNorm2d(out_channels)
+        else:
+            self.bn = None
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.relu(x)
-        x = self.bn(x)
+
+        # apply batch norm
+        if self.bn is not None:
+            x = self.bn(x)
         return x
 
 
@@ -21,14 +29,15 @@ class CNN(nn.Module):
     def __init__(self, num_classes, in_channels=3, input_size=224):
         super(CNN, self).__init__()
         conv_params = [
-            [in_channels, 16, 3],
-            [16, 32, 5],
-            [32, 64, 5],
-            [64, 128, 3],
-            [128, 256, 3]
+            # (in_channels, out_channels, kernel_size, batch_norm)
+            [in_channels, 16, 3, False],
+            [16, 32, 5, False],
+            [32, 64, 5, True],
+            [64, 128, 3, True],
+            [128, 256, 3, True]
         ]
         pooled_size = input_size // 2**len(conv_params)
-        dropout = (0, 0.2, 0.3, 0.5)
+        dropout = (0, 0.2, 0.3)
         classifier_params = [
             [256*pooled_size*pooled_size, 512],
             [512, 128],
@@ -36,37 +45,54 @@ class CNN(nn.Module):
             [64, num_classes]
         ]
 
-        self.classifier = self.make_classifier(
-            classifier_params, dropout=dropout)
+        #
         self.conv = self.make_backbone(conv_params)
 
+        #
+        self.classifier = self.make_classifier(
+            classifier_params, dropout=dropout)
+
     def forward(self, x):
+        # feed forward convolution's layers
         x = self.conv(x)
+
+        # feed forward classifier's layers
         x = self.classifier(x)
         return x
 
     @staticmethod
     def make_backbone(conv_params, max_pool=2):
         layers = []
-        for layer in conv_params:
-            in_channels, out_channels, kernel_size = layer
-            layers.append(ConvBlock(in_channels, out_channels, kernel_size))
+        #
+        for params in conv_params:
+            in_channels, out_channels, kernel_size, use_batch_norm = params
+            layers.append(ConvBlock(in_channels, out_channels,
+                          kernel_size, use_batch_norm))
             if max_pool:
                 layers.append(nn.MaxPool2d(kernel_size=max_pool))
 
         return nn.Sequential(*layers)
 
     @staticmethod
-    def make_classifier(classifier_params, dropout=(0, 0.2, 0.3, 0.5)):
+    def make_classifier(classifier_params, dropout=(0, 0.2, 0.3)):
         layers = [nn.Flatten()]
         #
-        for params in classifier_params:
+        for params in classifier_params[:-1]:
             in_size, out_size = params
             layers.append(nn.Linear(in_size, out_size))
             layers.append(nn.ReLU(inplace=True))
             layers.append(nn.Dropout(random.choice(dropout)))
 
+        # last layer with no Dropout
+        in_size, out_size = classifier_params[-1]
+        layers.append(nn.Linear(in_size, out_size))
         layers.append(nn.LogSoftmax(dim=1))
 
         #
         return nn.Sequential(*layers)
+
+
+# if __name__ == '__main__':
+#     cnn = CNN(num_classes=5)
+#     from torchsummary import summary
+#     print(summary(cnn, input_size=(3, 224, 224)))
